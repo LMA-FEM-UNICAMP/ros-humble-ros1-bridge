af89fe7e9f2b09c045acfecfb90b4124067af239
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2025-01-29 13:51:16 -0300

    Update launch

--- launch/ros1_dynamic_bridge_launch.py
+++ launch/ros1_dynamic_bridge_launch.py
@@ -38,11 +38,12 @@ def generate_launch_description():
     ros1_dynamic_bridge = launch_ros.actions.Node(package='ros1_bridge',
                                                    executable='dynamic_bridge',
                                                    output='screen',
-                                                   arguments=['--print-pairs',
-                                                            #   '--show-introspection',
-                                                            #   '--bridge-all-topics',
-                                                            #   '--bridge-all-1to2-topics',
-                                                            #   '--bridge-all-2to1-topics'
+                                                   arguments=[
+                                                              # '--print-pairs',
+                                                              # '--show-introspection',
+                                                              # '--bridge-all-topics',
+                                                              # '--bridge-all-1to2-topics',
+                                                              # '--bridge-all-2to1-topics'
                                                               ])
 
     return launch.LaunchDescription([ros1_dynamic_bridge
